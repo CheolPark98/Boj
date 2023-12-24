e953889f897a8604d86c11d5b2904335d6c6ebd5
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2023-12-25 05:51:04 +0900

    [Silver III] Title: N과 M (4), Time: 104 ms, Memory: 35680 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+import heapq
+import copy
+
+def dfs(n,lst,b):
+    if (n==M):
+
+        result.append(lst)
+        return
+    
+    for j in range(1,N+1):
+        if j>=b:
+            v[j]=1
+            dfs(n+1,lst+[j],j)
+            v[j]=0
+
+
+N,M=map(int,input().split())
+v=[0]*(N+1)
+result=[]
+dfs(0,[],0)
+
+for i in result:
+    print(*i)
