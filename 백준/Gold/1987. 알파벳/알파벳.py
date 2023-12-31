09c583a71a7459a930c70295792612887e466c84
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2023-12-31 12:09:48 +0900

    [Gold IV] Title: 알파벳, Time: 1448 ms, Memory: 239152 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -9,29 +9,27 @@ arr = [list(map(str, sys.stdin.readline().strip())) for _ in range(N)]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-v1 = [0] * 26
-v2 = [[0] * M for i in range(N)]
-v1[ord(arr[0][0]) - ord("A")] = 1
-v2[0][0] = 1
+
+def bfs():
+    q = deque()
+    q.append((0, 0, arr[0][0]))
+    v = [[set() for i in range(M)] for j in range(N)]
+    v[0][0].add(arr[0][0])
+    while q:
+        global maxn
+        x, y, s = q.popleft()
+        maxn = max(maxn, len(s))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            for i in range(4):
+                if 0 <= nx < N and 0 <= ny < M:
+                    if arr[nx][ny] not in s and s + arr[nx][ny] not in v[nx][ny]:
+                        v[nx][ny].add(s + arr[nx][ny])
+                        q.append((nx, ny, s + arr[nx][ny]))
+
+
 maxn = 0
-
-
-def dfs(x, y, cnt):
-    global maxn
-    if cnt > maxn:
-        maxn = cnt
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if 0 <= nx < N and 0 <= ny < M:
-            if v1[ord(arr[nx][ny]) - ord("A")] == 0 :
-                
-                v1[ord(arr[nx][ny]) - ord("A")] = 1
-                dfs(nx, ny, cnt + 1)
-               
-                v1[ord(arr[nx][ny]) - ord("A")] = 0
-
-
-dfs(0, 0, 1)
+bfs()
 print(maxn)
