703954fd21fc7c1f793f071dae7a2b81516609eb
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2023-12-31 13:40:31 +0900

    [Silver I] Title: 스타트링크, Time: 540 ms, Memory: 71772 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -4,8 +4,6 @@ from itertools import combinations
 
 F, S, G, U, D = map(int, input().split())
 v = [0] * 1000001
-v2 = []
-
 
 def bfs():
     q = deque()
@@ -14,16 +12,11 @@ def bfs():
         a = q.popleft()
         if a == G:
             break
-        if len(v2) > 2:
-            if v2[len(v2) - 1] == v2[len(v2) - 3]:
-                break
         for i in (U, -D):
             if 0 < i + a <= F:
                 if v[i + a] == 0 and i != 0:
                     v[i + a] = v[a] + 1
                     q.append(a + i)
-                    v2.append(a + i)
-
 
 a = bfs()
 if v[G] == 0 and S != G:
