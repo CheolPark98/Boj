f63e7a12694592df80074755340305228d339986
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-01-09 11:39:55 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 34060 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,41 @@
+from collections import deque
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = {}
+
+for i in range(n):
+    p, l, r = map(str, input().split())
+    arr[p] = [l, r]
+
+
+def preorder(root):
+    if root != ".":
+        print(root, end="")
+        preorder(arr[root][0])
+        preorder(arr[root][1])
+
+
+def inorder(root):
+    if root != ".":
+        inorder(arr[root][0])
+        print(root, end="")
+        inorder(arr[root][1])
+
+
+def postorder(root):
+    if root != ".":
+        postorder(arr[root][0])
+        postorder(arr[root][1])
+        print(root, end="")
+
+
+preorder("A")
+print()
+inorder("A")
+print()
+postorder("A")
