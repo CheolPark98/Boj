3f42e4ac36356fb98e4f2eac9b3aee2e84238d71
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-03-01 22:22:48 +0900

    [Gold III] Title: 앱, Time: 612 ms, Memory: 48568 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -6,20 +6,18 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 b = [0] + list(map(int, input().split()))
 c = [0] + list(map(int, input().split()))
-sm = 0
-for i in range(n + 1):
-    if c[i] == 0:
-        sm += b[i]
 
-dp = [[0 for i in range(sum(c) + 1)] for j in range(n + 1)]
+dp = [[0 for i in range(sum(c) + 1)] for i in range(n+1)]
 mn = sum(c)
 for i in range(1, n + 1):
-    for j in range(sum(c) + 1):
+    for j in range( sum(c) + 1):
         if j < c[i]:
             dp[i][j] = dp[i - 1][j]
         else:
             dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - c[i]] + b[i])
         if dp[i][j] >= m:
             mn = min(j, mn)
-print(mn)
-
+if m == 0:
+    print(0)
+else:
+    print(mn)
